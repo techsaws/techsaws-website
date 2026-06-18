d729e5c65555ee020a1afc392aae08f38ed43bf4
Syed Hassan Ali <syeddhassannaali@gmail.com>
2026-06-19 01:09:40 +0500

    made changes

--- src/components/layouts/sticky-robot.tsx
+++ src/components/layouts/sticky-robot.tsx
@@ -2,17 +2,17 @@
 
 import Lottie from "lottie-react";
 
-import RobotAnimation from "../../../public/animations/loading.json";
+import RobotAnimation from "../../../public/animations/robot.json";
 
 export default function StickyRobot() {
   return (
     <div
-      className="fixed bottom-9 right-1/2 z-40 pointer-events-none select-none"
+      className="fixed bottom-9 right-8 z-40 pointer-events-none select-none"
       aria-hidden="true"
     >
       <div
-        className="md:w-24
-          md:h-24
+        className="md:w-28
+          md:h-28
           w-18
           h-18
           transform-gpu
